b63027079a65cf8457790c8d5ee553ffb36a2b58
rjptegelaar <Gebruiker@LAPTOP-PROGAIA>
2017-06-06 15:03:28 +0200

    New API

--- src/main/java/nl/paultegelaar/liquid/relay/fuse/LiquidComponent.java
+++ src/main/java/nl/paultegelaar/liquid/relay/fuse/LiquidComponent.java
@@ -24,4 +24,6 @@ public class LiquidComponent extends UriEndpointComponent {
         setProperties(endpoint, parameters);
         return endpoint;
     }
+    
+    
 }
--- src/main/java/nl/paultegelaar/liquid/relay/fuse/LiquidProducer.java
+++ src/main/java/nl/paultegelaar/liquid/relay/fuse/LiquidProducer.java
@@ -56,5 +56,7 @@ public class LiquidProducer extends DefaultProducer {
     		transport.destroy();
 		}    
     }
+    
+    
 
 }
